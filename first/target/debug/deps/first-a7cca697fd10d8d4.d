b62a7f01091d34a77f44a6638f7d8f0231e10acc
codecraft26 <gaman0221@gmial.com>
2023-12-06 20:09:43 +0530

    first push

---
+++ first/target/debug/deps/first-a7cca697fd10d8d4.d
@@ -0,0 +1,5 @@
+/home/codecraft/Desktop/rust_projects/first/target/debug/deps/first-a7cca697fd10d8d4: src/main.rs
+
+/home/codecraft/Desktop/rust_projects/first/target/debug/deps/first-a7cca697fd10d8d4.d: src/main.rs
+
+src/main.rs:
---
+++ first/target/debug/first.d
@@ -0,0 +1,1 @@
+/home/codecraft/Desktop/rust_projects/first/target/debug/first: /home/codecraft/Desktop/rust_projects/first/src/main.rs
